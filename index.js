acce0bd34308bfd2861f807481b86451dd43f084
mark <max.strubinger@gmail.com>
2022-07-10 05:14:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function revealY() {
+  var reveals = document.querySelectorAll(".revealY");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 25;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+function revealX() {
+  var reveals = document.querySelectorAll(".revealX");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 25;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealY);
+window.addEventListener("scroll", revealX);
+
+var introJson = $('#introJson');
+
+setTimeout(function(){
+  introJson.removeClass('typewriter-text');
+}, 2500);
